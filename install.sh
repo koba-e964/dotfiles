3cb1eb9ddbe4a0240f0d1b0e803f3ac561387d97
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2026-01-02 23:15:49 +0900

    Make install.sh more quiet

--- install.sh
+++ install.sh
@@ -9,19 +9,20 @@ if [ "$(uname)" == "Darwin" ]; then
     if ! command -v brew >/dev/null; then
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
-    brew install git openssl@3 git-lfs yq jq go git-crypt binutils binwalk openssh ghc python@3 nodebrew sqlite rbenv ruby-build
+    brew install --quiet \
+        git openssl@3 git-lfs yq jq go git-crypt binutils binwalk openssh ghc graphviz llvm python@3 nodebrew sqlite rbenv ruby-build xz
 else
     # Linux
     true
 fi
-if ! command -v rustup; then
+if ! command -v rustup >/dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 fi
 
 ./git/install.sh
 
 # https://nix.dev/manual/nix/2.33/installation/installing-binary
-if ! command -v nix; then
+if ! command -v nix >/dev/null; then
     NIX_VERSION=2.33.0
     curl -L https://releases.nixos.org/nix/nix-${NIX_VERSION}/install | sh
 fi
