fe27ab461b1078755f8fff90ed5a20a5143ea4dc
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2026-01-08 15:23:02 +0900

    Fix indent (2 -> 4)

--- flake.nix
+++ flake.nix
@@ -1,24 +1,24 @@
 {
-  description = "dotfiles (home-manager, macOS)";
+    description = "dotfiles (home-manager, macOS)";
 
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
+    inputs = {
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
     };
-  };
 
-  outputs = { self, nixpkgs, home-manager, ... }:
-  let
-    system = "aarch64-darwin";
-    pkgs = import nixpkgs { inherit system; };
-  in
-  {
-    homeConfigurations.default =
-      home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home/home.nix ];
-      };
-  };
+    outputs = { self, nixpkgs, home-manager, ... }:
+    let
+        system = "aarch64-darwin";
+        pkgs = import nixpkgs { inherit system; };
+    in
+    {
+        homeConfigurations.default =
+            home-manager.lib.homeManagerConfiguration {
+                inherit pkgs;
+                modules = [ ./home/home.nix ];
+            };
+    };
 }
--- home/home.nix
+++ home/home.nix
@@ -1,16 +1,16 @@
 { config, pkgs, ... }:
 
 {
-  home.username = builtins.getEnv "USER";
-  home.homeDirectory = builtins.getEnv "HOME";
+    home.username = builtins.getEnv "USER";
+    home.homeDirectory = builtins.getEnv "HOME";
 
-  home.stateVersion = "24.05";
+    home.stateVersion = "24.05";
 
-  # programs.zsh.enable = true;
-  # programs.git.enable = true;
+    # programs.zsh.enable = true;
+    # programs.git.enable = true;
 
-  home.packages = with pkgs; [
-    ripgrep
-    fd
-  ];
+    home.packages = with pkgs; [
+        ripgrep
+        fd
+    ];
 }
