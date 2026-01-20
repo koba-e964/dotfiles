b7c930ee132990ce18189e23f444fe735aea58fc
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2026-01-20 10:20:12 +0900

    Make ripgrep search .github/

--- home/home.nix
+++ home/home.nix
@@ -12,10 +12,20 @@
     programs = {
         zsh = import ./zsh;
         git = import ./git;
+        ripgrep = {
+            enable = true;
+            arguments = [
+                "--hidden"
+                "--glob=!.git/**"
+                "--glob=!.env/**"
+            ];
+        };
     };
 
+    home.sessionVariables.RIPGREP_CONFIG_PATH =
+        "${config.xdg.configHome}/ripgrep/ripgreprc";
+
     home.packages = with pkgs; [
-        ripgrep
         fd
         stow
     ];
