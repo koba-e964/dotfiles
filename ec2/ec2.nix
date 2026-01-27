1764dd9ec71d230ac7f82b29b88107fc837d711d
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2026-01-27 20:04:48 +0900

    Add tmux config

--- ec2/ec2.nix
+++ ec2/ec2.nix
@@ -1,13 +1,22 @@
 { config, pkgs, ... }:
 
 {
+    home.username = builtins.getEnv "USER";
+    home.homeDirectory = builtins.getEnv "HOME";
+
+    home.stateVersion = "24.05";
+
+    programs = {
+        tmux = {
+            enable = true;
+        };
+    };
     home.packages = with pkgs; [
         codex
         fd
         git
         ripgrep
         stow
-        termux
         pre-commit
     ];
 }
