98f943872eebfd7350dffa27e9c1175acd551369
koba-e964 <3303362+koba-e964@users.noreply.github.com>
2026-02-16 10:47:29 +0900

    Install ruff

--- tentative-install-python.sh
+++ tentative-install-python.sh
@@ -1,3 +1,3 @@
 # TODO: Manage local_python by another means
 # This script handles installations to local_python (~/local_python)
-pip3 install podman-compose==1.5.0 pip==26.0.1
+pip3 install podman-compose==1.5.0 pip==26.0.1 ruff==0.15.1
